228598d6bead5c4700c6af9c855757d17d051c6d
Bharat Byan <bharat.byan@lfbn-1-12791-146.w90-90.abo.wanadoo.fr>
2018-02-24 08:27:12 +0530

    -Changes and optimisation with new delete features. -Fixes like AR not opening in some devices, status bar getting hidden etc.

---
+++ XCoreData/XCoreData/ViewControllers/XAddressListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  XAddressListViewController.swift
+//  XCoreData
+//
+//  Created by bharat byan on 24/02/18.
+//  Copyright © 2018 Bharat Byan. All rights reserved.
+//
+
+import UIKit
+
+class XAddressListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension XAddressListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "CellAddressList")
+        
+        return cell
+    }
+}
